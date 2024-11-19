9f473c143d75aefd1d93b60b03a655fbee67018c
shiyinxi <shiyinxi@gmail.com>
2024-11-19 10:01:12 -0800

    updates

--- pig.py
+++ pig.py
@@ -1,7 +1,13 @@
 def pig_latin(sentence):
     # Write your solution here!
-    pass
+    word_list = sentence.split()
+    for i in range(len(word_list)):
+        if word_list[i][0] not in "aeiou":
+            word_list[i] = word_list[i][1:] + word_list[i][0] + 'ay'
+        
+    return " ".join(word_list)
 
+print(pig_latin("latin is a hard language"))
 # Test cases
 assert pig_latin("something") == "omethingsay"
 assert pig_latin("awesome") == "awesome"
@@ -11,3 +17,4 @@ assert pig_latin("e") == "e"
 
 print("All tests passed!")
 print("Discuss time and space complexity if there's time remaining")
+
